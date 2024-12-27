1d4fde290ddb7b63216ed1f16f6598e8b22b1167
TechNing39 <9022976@naver.com>
2024-12-27 21:55:27 +0900

    [Feat] FloatingButtonView 뷰 구현

--- app/src/main/java/com/mechuu/miniin/MainActivity.kt
+++ app/src/main/java/com/mechuu/miniin/MainActivity.kt
@@ -22,11 +22,5 @@ class MainActivity : AppCompatActivity() {
             val intent= Intent(this,ChatActivity::class.java)
             startActivity(intent)
         }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
