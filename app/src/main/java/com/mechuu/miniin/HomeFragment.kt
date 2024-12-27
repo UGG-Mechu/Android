0d31e1fefd158c4ad709d3f1eed1ae9a0c221546
TechNing39 <9022976@naver.com>
2024-12-28 04:23:55 +0900

    [feat] billActivity

--- app/src/main/java/com/mechuu/miniin/HomeFragment.kt
+++ app/src/main/java/com/mechuu/miniin/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.mechuu.miniin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mechuu.miniin.BillActivity
 import com.mechuu.miniin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -13,9 +15,15 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        binding.todaySpend.setOnClickListener {
+            val intent = Intent(activity, BillActivity::class.java)
+            startActivity(intent)
+        }
 
         return binding.root
     }
